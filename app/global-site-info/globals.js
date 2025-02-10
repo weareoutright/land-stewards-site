86f7e7b48374a0739c2f6d5dd5968a33ed319cff
Dakota Fabro <dakota@weareoutright.com>
2025-02-10 12:00:19 -0800

    implement initial file structure

---
+++ app/global-site-info/globals.js
@@ -0,0 +1,14 @@
+export const GLOBALS = {
+  title: "Land Stewards Foundation",
+  description: "Cultivating Thriving Landscapes",
+  fonts: {
+    geistSans: {
+      variable: "--font-geist-sans",
+      subsets: ["latin"],
+    },
+    geistMono: {
+      variable: "--font-geist-mono",
+      subsets: ["latin"],
+    },
+  },
+};
