740051ab0e03d880566538c3962fe703cf2b8cd7
Dakota Fabro <dakota@weareoutright.com>
2025-02-10 14:24:18 -0800

    implement hero

--- app/components/home/Hero.jsx
+++ app/components/home/Hero.jsx
@@ -1,9 +1,7 @@
 import React from "react";
+import Image from "next/image";
 import Iframe from "react-iframe";
-
-{
-  /* <div style="padding:75% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/1055359690?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="home-header-video"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script> */
-}
+import WAVE from "../../assets/wave.svg";
 
 const Hero = () => {
   return (
@@ -15,8 +13,18 @@ const Hero = () => {
           className="background-video"
           allow="autoplay"
         ></Iframe>
+        <Image src={WAVE} alt="wave" className="wave" />
       </div>
-      <h1 className="header-1">Cultivating Thriving Landscapes</h1>
+      <div className="content home-hero-content">
+        <h1 className="header-1">
+          Cultivating Thriving<br></br> Landscapes
+        </h1>
+        <p className="hero-text">
+          We promote innovative stewardship practices using a land-as-laboratory
+          approach that honors the site-specific needs of the land, its history,
+          and its communities.
+        </p>
+      </div>
     </div>
   );
 };
