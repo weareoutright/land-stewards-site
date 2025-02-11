88127be3ea52ce7898bd2e87a08cfbced8611151
Dakota Fabro <dakota@weareoutright.com>
2025-02-10 20:25:01 -0800

    add disabled btn scss rules; clean up page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import Hero from "./components/home/Hero";
-import Footer from "./components/Footer";
 import CTABannerHome from "./components/home/CTABannerHome";
 import HomeContent from "./components/home/HomeContent";
 
