eca96fa6de556cc5a0ba029448a3407684bec421
Dakota Fabro <dakota@weareoutright.com>
2025-02-10 17:46:16 -0800

    added photos; styled the cta banner bg

---
+++ app/components/CTABannerHome.jsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import CTA_HOME_BG from "../assets/home/footer-cta-bg.svg";
+
+const WAYPOINTS = [
+  {
+    id: "partner-with-us",
+    description:
+      "We collaborate with people and organizations who share our goals. We invite research and pilot projects onto our demonstration site, and we travel to visit other sites as well.",
+    btnText: "Partner With Us",
+    href: "/partner-learn",
+  },
+  {
+    id: "get-in-touch",
+    description:
+      "We inspire landowners and managers (large and small) with steps they can take on their lands. We offer site visits, workshops and training programs, and other learning opportunities.",
+    btnText: "Get In Touch",
+    href: "/contact",
+  },
+  {
+    id: "ways-to-give",
+    description:
+      "We are a nonprofit organization and welcome support for our mission from institutions and individuals. If your funding priorities align with our work, let’s get in touch.",
+    btnText: "Ways To Give",
+    href: "/ways-to-give",
+  },
+];
+
+const CTABannerHome = () => {
+  return (
+    <div className="CTABannerHome">
+      <div className="cta-bg"></div>
+      <small>Make an impact</small>
+      <h2>Get Involved</h2>
+      <div className="cta-waypoints">
+        {WAYPOINTS.map((waypoint) => (
+          <div key={waypoint.id} className="cta-waypoint">
+            <p>{waypoint.description}</p>
+            <Link href={waypoint.href} className="waypoint-btn primary-btn">
+              {waypoint.btnText}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CTABannerHome;
--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "./components/home/Hero";
 import Footer from "./components/Footer";
+import CTABannerHome from "./components/CTABannerHome";
 
 export default function Home() {
   return (
     <div className="Home">
       <main>
         <Hero />
+        <CTABannerHome />
       </main>
     </div>
   );
