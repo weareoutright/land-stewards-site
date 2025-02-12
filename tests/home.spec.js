fb6b07468a3f93afb5fedad49de0409dbbb58d7a
Dakota Fabro <dakota@weareoutright.com>
2025-02-12 13:31:52 -0800

    implement sass migrator to avoid breaking changes

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -2,5 +2,5 @@ import { test, expect } from "@playwright/test";
 
 test("homepage should have the correct title", async ({ page }) => {
   await page.goto("/");
-  await expect(page).toHaveTitle(/Next.js App/);
+  await expect(page).toHaveTitle(/Land Stewards Foundation/);
 });
