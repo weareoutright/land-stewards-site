76b3f8d765aff524d2051b89ec65ecc28c02c315
Dakota Fabro <dakota@weareoutright.com>
2025-02-20 15:44:31 -0800

    refactored carousel items; added education

--- app/components/home/HomeContent.jsx
+++ app/components/home/HomeContent.jsx
@@ -4,37 +4,7 @@ import CURRENT_RESEARCH from "../../assets/home/current-research.svg";
 import LAND_STEWARDS from "../../assets/home/land-stewards.svg";
 import CardSideBySide from "../CardSideBySide";
 import Carousel from "../Carousel";
-import EASTERN_HEMLOCK_RESTORATION from "../../assets/carousel/eastern-hemlock.svg";
-import REGEN_AGRICULTURE from "../../assets/carousel/regen.svg";
-import NATIVE_PLANT_NURSERY from "../../assets/carousel/native-plant-nursery.svg";
-import WETLANDS from "../../assets/carousel/wetlands.svg";
-
-const CAROUSEL_ITEMS = [
-  {
-    id: "eastern-hemlock-restoration",
-    image: EASTERN_HEMLOCK_RESTORATION,
-    title: "Saving the Eastern Hemlock",
-    href: "/current-research/hemlock-restoration",
-  },
-  {
-    id: "regen-agriculture",
-    image: REGEN_AGRICULTURE,
-    title: "Regenerative Agriculture",
-    href: "/biodivesity-and-rewilding/regenerative-agriculture",
-  },
-  {
-    id: "native-plant-nursery",
-    image: NATIVE_PLANT_NURSERY,
-    title: "Native Plant Nursery",
-    href: "/biodivesity-and-rewilding/hemlock-restoration",
-  },
-  {
-    id: "wetlands-restoration",
-    image: WETLANDS,
-    title: "Wetlands Restoration",
-    href: "/biodivesity-and-rewilding/wetlands-restoration",
-  },
-];
+import { CAROUSEL_ITEMS } from "../../constants/CAROUSEL_ITEMS";
 
 const HomeContent = () => {
   return (
---
+++ app/constants/CAROUSEL_ITEMS.js
@@ -0,0 +1,38 @@
+import EASTERN_HEMLOCK_RESTORATION from "../assets/carousel/eastern-hemlock.svg";
+import REGEN_AGRICULTURE from "../assets/carousel/regen.svg";
+import NATIVE_PLANT_NURSERY from "../assets/carousel/native-plant-nursery.svg";
+import WETLANDS from "../assets/carousel/wetlands.svg";
+import EDUCATION from "../assets/carousel/education.svg";
+
+export const CAROUSEL_ITEMS = [
+  {
+    id: "eastern-hemlock-restoration",
+    image: EASTERN_HEMLOCK_RESTORATION,
+    title: "Saving the Eastern Hemlock",
+    href: "/current-research/hemlock-restoration",
+  },
+  {
+    id: "regen-agriculture",
+    image: REGEN_AGRICULTURE,
+    title: "Regenerative Agriculture",
+    href: "/biodivesity-and-rewilding/regenerative-agriculture",
+  },
+  {
+    id: "native-plant-nursery",
+    image: NATIVE_PLANT_NURSERY,
+    title: "Native Plant Nursery",
+    href: "/biodivesity-and-rewilding/hemlock-restoration",
+  },
+  {
+    id: "wetlands-restoration",
+    image: WETLANDS,
+    title: "Wetlands Restoration",
+    href: "/biodivesity-and-rewilding/wetlands-restoration",
+  },
+  {
+    id: "education",
+    image: EDUCATION,
+    title: "Education",
+    href: "/education",
+  },
+];
